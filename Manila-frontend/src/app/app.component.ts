cb92ce26e278e3eb3cb4dd18275d55ee731bad61
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2024-12-05 09:40:15 -0500

    Creacion de clase navegador e inicio

--- Manila-frontend/src/app/app.component.ts
+++ Manila-frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavegadorComponent } from './navegador/navegador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioComponent, NavegadorComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Manila-frontend/src/app/navegador/navegador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegador',
+  standalone: true,
+  imports: [],
+  templateUrl: './navegador.component.html',
+  styleUrl: './navegador.component.css'
+})
+export class NavegadorComponent {
+
+}
