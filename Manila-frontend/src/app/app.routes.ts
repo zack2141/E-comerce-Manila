68170c496927cbc099699afcccd3945e499a4a25
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2024-12-11 08:48:42 -0500

    diseño de la pagina de inicio

--- Manila-frontend/src/app/app.routes.ts
+++ Manila-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProdctosComponent } from './prodctos/prodctos.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 export const routes: Routes = [
-    {path:'productos', component:ProdctosComponent}
+    {path:'productos', component:ProdctosComponent},
+    {path:'carrito', component:CarritoComponent}
 ];
 
 export class AppRoutingModule{}
