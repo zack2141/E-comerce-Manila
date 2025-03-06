2c09f39450a740f966394be82d5a3a9c85654385
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-03-06 14:06:59 -0500

    primer commit creacion del backend

---
+++ Manila-backend/src/main/java/com/example/demo/ManilaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManilaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManilaBackendApplication.class, args);
+	}
+
+}
