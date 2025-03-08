a2fdf468f3a642049aebb068d49885c9f940167a
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-03-08 12:21:32 -0500

    otra entidad

---
+++ Manila-frontend/src/app/otra/otra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otra',
+  standalone: true,
+  imports: [],
+  templateUrl: './otra.component.html',
+  styleUrl: './otra.component.css'
+})
+export class OtraComponent {
+
+}
